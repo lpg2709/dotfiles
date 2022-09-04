84af99750a849c26cd2957732affcfa1403aef9f
Leonardo  Padilha Guarezi <leonardo_guarezi@hotmail.com>
2022-09-04 20:47:04 +0000

    FIX

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ function check_execution(){
 
 USER_NAME="${SUDO_USER:-$USER}"
 USER_HOME="/home/$USER_NAME"
-BASE_DIR=$(echo "${BASH_SOURCE}" | sed 's/install.sh/'})
+BASE_DIR=$(echo "${BASH_SOURCE}" | sed 's/install.sh//'})
 echo "BASE: $BASE_DIR"
 if [ ! -d "$USER_HOME" ]; then
 	printc "User not found!\n" "e"
