6146fd881b65cfde6ee5eff4aaf84d85c4398175
Leonardo  Padilha Guarezi <leonardo_guarezi@hotmail.com>
2023-08-28 14:01:04 +0000

    FIX hidden files on file_search, remove unused function

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -14,6 +14,12 @@ telescope.setup {
             }
         }
     },
+	pickers = {
+		find_files = {
+			file_ignore_patterns = { "^node_modules/", "^undo/" },
+			hidden = true,
+		}
+	},
 	extensions = {
 		fzf = {
 			fuzzy = true,                    -- false will only do exact matching
@@ -28,26 +34,4 @@ telescope.setup {
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 telescope.load_extension('fzf')
-
-local M = {}
-
-M.search_dotfiles = function()
-    require('telescope.builtin').find_files({
-        prompt_title = "<.dotfiles>",
-        cwd = '~/.dotfiles',
-        hidden = true
-    })
-end
-
-M.project_files = function()
-    local opts = {}
-    local ok = pcall(require('telescope.builtin').git_files, opts)
-    if not ok then require('telescope.builtin').find_files(opts) end
-end
-
-vim.api.nvim_set_keymap('n', '<Leader>p', ':lua require\'telescope\'.project_files()<Cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Leader>df', ':lua require\'telescope\'.search_dotfiles()<Cr>', {noremap = true, silent = true})
-
-return M
-
 
