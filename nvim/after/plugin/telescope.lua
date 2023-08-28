efa75623f89d8965aa048db6907d6fe186ba562b
Leonardo  Padilha Guarezi <leonardo_guarezi@hotmail.com>
2023-08-28 14:02:30 +0000

    FIX identation

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,18 +2,18 @@ local telescope = require('telescope')
 local actions = require('telescope.actions')
 
 telescope.setup {
-    defaults = {
-        color_devicons = false,
-        prompt_prefix = "$ ",
-        file_ignore_patterns = { "^.git/" },
-        mappings = {
-            i = {
-                ['<esc>'] = actions.close,
+	defaults = {
+		color_devicons = false,
+		prompt_prefix = "$ ",
+		file_ignore_patterns = { "^.git/" },
+		mappings = {
+			i = {
+				['<esc>'] = actions.close,
 				['<C-j>'] = actions.move_selection_next,
 				['<C-k>'] = actions.move_selection_previous
-            }
-        }
-    },
+			}
+		}
+	},
 	pickers = {
 		find_files = {
 			file_ignore_patterns = { "^node_modules/", "^undo/" },
@@ -26,7 +26,7 @@ telescope.setup {
 			override_generic_sorter = true,  -- override the generic sorter
 			override_file_sorter = true,     -- override the file sorter
 			case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-											 -- the default case_mode is "smart_case"
+			-- the default case_mode is "smart_case"
 		}
 	}
 }
