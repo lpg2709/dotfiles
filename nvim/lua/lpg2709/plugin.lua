1a2c9c9e7cb56821833a4eb5234205803b39948f
Leonardo Padilha Guarezi <leonardo_guarezi@hotmail.com>
2024-02-08 14:15:22 -0300

    ADD MinifyJSON

--- nvim/lua/lpg2709/plugin.lua
+++ nvim/lua/lpg2709/plugin.lua
@@ -1,14 +1,30 @@
 -- File with functions usefull for me
 
--- Pretty JSON format
-function PrettyJSON()
+function find_jq()
 	vim.fn.system('jq --version')
 	if vim.v.shell_error == 1 then
 		vim.notify("ERROR: jq command not found!")
-		return
+		return false
 	end
-	vim.cmd("%!jq .")
-	vim.bo.filetype="json"
-	vim.notify("JSON formated")
+
+	return true
+end
+
+-- Pretty JSON format
+function PrettyJSON()
+	if find_jq() then
+		vim.cmd("%!jq .")
+		vim.bo.filetype="json"
+		vim.notify("JSON formated")
+	end
 end
 vim.cmd([[command! PrettyJSON lua PrettyJSON()]])
+
+-- Minify JSON format
+function MinifyJSON()
+	if find_jq() then
+		vim.cmd("%!jq -c .")
+		vim.notify("JSON formated")
+	end
+end
+vim.cmd([[command! MinifyJSON lua MinifyJSON()]])
