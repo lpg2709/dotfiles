31038ceade4b330080a05a4d21fe74c56f0f9e4a
Leonardo  Padilha Guarezi <leonardo_guarezi@hotmail.com>
2024-02-14 20:10:57 +0000

    Update treesitter.lua

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,7 +1,18 @@
 local treesitter = require('nvim-treesitter.configs')
 
 treesitter.setup {
-	ensure_installed = { "bash", "c", "cmake", "cpp", "glsl", "lua", "rust", "vim", "vimdoc" },
+	ensure_installed = { 
+--        "bash", 
+--        "c", 
+--        "cmake", 
+--        "cpp", 
+--        "glsl", 
+        "lua", 
+--        "rust", 
+--        "vim", 
+        "vimdoc",
+--        "zig"
+    },
     highlight = {
         enable = true
     }
