b34b2107eb85ad8fb6e9fa7ff83573ec66739476
lpg2709 <leonardo_guarezi@hotmail.com>
2025-04-25 11:17:12 -0300

    Remove unused configs; Update alacritty

--- install-win.bat
+++ install-win.bat
@@ -5,14 +5,8 @@ echo "Starting copy configurations"
 echo "-- Copy alacritty config to %AppData%\alacritty"
 xcopy alacritty "%AppData%\alacritty\*" /E /Y
 
-echo "-- Copy wezterm config to %USERPROFILE%"
-xcopy wezterm\.wezterm.lua "%USERPROFILE%" /E /Y
-
 echo "-- Copy nvim config to %LocalAppData%\nvim"
 xcopy nvim "%LocalAppData%\nvim\*" /E  /Y
-
-echo "-- Copy vifm config to %AppData%\Vifm"
-xcopy vifm "%AppData%\Vifm\*" /E /Y
 
 echo "Finished ..."
 pause
--- uninstall-win.bat
+++ uninstall-win.bat
@@ -5,15 +5,9 @@ echo "Uninstalling configurations"
 echo "-- Uninstall alacritty config from %AppData%\alacritty"
 rmdir /s /q "%AppData%\alacritty"
 
-echo "-- Uninstall wezterm config from %USERPROFILE%"
-del /s /q "%USERPROFILE%\.wezterm.lua"
-
 echo "-- Uninstall nvim config from %LocalAppData%\nvim"
 rmdir /s /q "%LocalAppData%\nvim"
 rmdir /s /q "%LocalAppData%\nvim-data"
-
-echo "-- Uninstall vifm config from %AppData%\Vifm"
-rmdir /s /q "%AppData%\Vifm"
 
 echo "Finished ..."
 pause
